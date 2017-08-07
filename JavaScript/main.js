dc337c730b0129846d47ef980de55d1a2290e472
Aaron Vale <vale.aaron@gmail.com>
2017-08-06 20:21:33 -0400

    Added randomized cards and reset function

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -22,24 +22,60 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-function checkForMatch(){
-       if (cardsInPlay.length === 2){
+function checkForMatch(){   
+    if (cardsInPlay.length === 2){
         if (cardsInPlay[0] === cardsInPlay[1]){
             alert("You found a match!");
+            setTimeout(function(){
+                var gameBoard = document.getElementById("game-board");
+                while (gameBoard.hasChildNodes()) {
+                    gameBoard.removeChild(gameBoard.lastChild);
+                }
+                cardsInPlay = [];
+                createBoard();
+            }, 2000);
         }
         else{
             alert("Sorry, try again.");
+            setTimeout(function(){
+                var imgs = document.getElementsByTagName("img");
+                for (var i = 0; i<imgs.length;i++){
+                    imgs[i].setAttribute('src','images/back.png');
+                    imgs[i].setAttribute('flipped','false');
+            }
+                cardsInPlay = [];  
+            },2000);
         }
     }  
 }
 
-function flipCard(cardId){
-    console.log("User flipped "+cards[cardId].rank);
-    cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].suit);
-    console.log(cards[cardId].cardImage);
-    checkForMatch();
+var flipCard = function(){  
+    if (this.getAttribute('flipped') === 'false'){
+        var cardId = this.getAttribute('data-id');
+        cardsInPlay.push(cards[cardId].rank);
+        this.setAttribute('src',cards[cardId].cardImage);
+        this.setAttribute('flipped',true);
+        checkForMatch();
+    }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+    var array = [0,1,2,3];
+    var cardOrder = [];
+    for (var i = 0; i<cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.setAttribute('flipped','false');
+        cardElement.addEventListener('click', flipCard);
+        var orderNumber = array[Math.floor(Math.random() * array.length)];
+        var index = array.indexOf(orderNumber);
+        array.splice(index,1);
+        cardOrder[orderNumber] = cardElement;
+    } 
+    for (var j=0;j<cardOrder.length;j++){
+        document.getElementById("game-board").appendChild(cardOrder[j]);
+    }
+}
+
+createBoard();
